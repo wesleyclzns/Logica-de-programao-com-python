b50da0b154d55560f592adf611918a674031903f
Wesley Café Calazans <wesclzns@gmail.com>
2019-10-20 23:46:11 -0300

    upload

---
+++ sucessor_antecessor.py
@@ -0,0 +1,13 @@
+#coding=UTF-8
+
+numSucessor = None
+numAntercessor = None
+numEscolhido = None
+
+	print "Escolha um numero inteiro"
+	numEscolhido = input()
+
+numSucessor = numEscolhido +1
+numAntecessor = numEscolhido -1
+
+	print "O Sucessor do numero {} é {} e o antecessor do mesmo é {}.".format(numEscolhido, numSucessor, numAntecessor)
